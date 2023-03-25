0ec92e7af701e0f3f80f1bc38bd98b86385c4740
gabrielyin <2021gabrielyin@gmail.com>
2023-03-25 12:14:36 -0300

    single post

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,3 +3,6 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+// https://www.youtube.com/watch?v=VXaYr6bI1pE
+// 30:14
